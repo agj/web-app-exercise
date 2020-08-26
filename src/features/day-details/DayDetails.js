825087faf9dbe203a2d4d7a9bc63d3f724f61188
agj <ale@agj.cl>
2020-08-26 00:05:16 -0400

    Personalizando gráfico de temperaturas.

--- src/features/day-details/DayDetails.js
+++ src/features/day-details/DayDetails.js
@@ -1,13 +1,21 @@
 
 import React from 'react';
 import styles from './DayDetails.module.css';
-import { letterFrequency } from '@vx/mock-data';
 import { Group } from '@vx/group';
-import { Bar } from '@vx/shape';
-import { scaleLinear, scaleBand } from '@vx/scale';
+import { LinePath, Circle } from '@vx/shape';
+import { scaleLinear } from '@vx/scale';
+import { pipe, min, max } from 'ramda';
 
 
-const data = letterFrequency;
+
+const data = [
+  { time: 0, temperature: 12 },
+  { time: 1, temperature: 9 },
+  { time: 2, temperature: 8 },
+  { time: 3, temperature: 6 },
+  { time: 4, temperature: 2 },
+  { time: 5, temperature: 1 },
+];
 
 const width = 500;
 const height = 500;
@@ -22,40 +30,33 @@ const margin = {
 const xMax = width - margin.left - margin.right;
 const yMax = height - margin.top - margin.bottom;
 
-const x = (d) => d.letter;
-const y = (d) => +d.frequency * 100;
+const x = (d) => d.time;
+const y = (d) => d.temperature;
 
 console.log(data);
 
-const xScale = scaleBand({
+const xScale = scaleLinear({
   range: [0, xMax],
-  round: true,
-  domain: data.map(x),
-  padding: 0.4,
+  domain: [data.map(x).reduce(min), data.map(x).reduce(max)],
 });
 const yScale = scaleLinear({
   range: [yMax, 0],
-  round: true,
-  domain: [0, Math.max(...data.map(y))],
+  domain: [data.map(y).reduce(min), data.map(y).reduce(max)],
 });
 
-const compose = (scale, accessor) => (data) => scale(accessor(data));
-const xPoint = compose(xScale, x);
-const yPoint = compose(yScale, y);
+const xPoint = pipe(x, xScale);
+const yPoint = pipe(y, yScale);
 
-const bars =
+const dots =
   data.map((d, i) => {
-    const barHeight = yMax - yPoint(d);
     return (
-      <Group key={ `bar-${ i }` }>
-        <Bar
-          x={ xPoint(d) }
-          y={ yMax - barHeight }
-          height={ barHeight }
-          width={ xScale.bandwidth() }
-          fill="red"
-        />
-      </Group>
+      <Circle
+        key={ `dot-${ x(d) }` }
+        cx={ xPoint(d) }
+        cy={ yPoint(d) }
+        r="10"
+        fill="red"
+      />
     )
   });
 
@@ -64,9 +65,20 @@ export function DayDetails(props) {
     <div className={ styles.DayDetails }>
       <svg width={ width } height={ height }>
         <Group top={ margin.top } left={ margin.left }>
-          { bars }
+          <LinePath
+            data={ data }
+            x={ pipe(x, xScale) }
+            y={ pipe(y, yScale) }
+            stroke="red"
+            strokeWidth={ 1.5 }
+            shapeRendering="geometricPrecision"
+          />
+          <Group>
+            { dots }
+          </Group>
         </Group>
       </svg>
     </div>
   );
 }
+
