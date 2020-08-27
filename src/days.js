52067066b46ffb634b5ab0f2a7f21b099874efd4
agj <ale@agj.cl>
2020-08-27 13:17:46 -0400

    Nombres de días ahora corresponden al real.

--- src/days.js
+++ src/days.js
@@ -1,11 +1,26 @@
+import { range } from 'ramda';
 
-export const dayNames = [
+const weekDays = [
+  'Domingo',
   'Lunes',
   'Martes',
   'Miércoles',
   'Jueves',
   'Viernes',
+  'Sábado',
 ];
+
+const today = (new Date()).getDay();
+
+export const dayNames =
+  range(0, 5)
+  .map((i) => {
+    const index = (today + i) % weekDays.length;
+    const dayName = weekDays[index];
+    return i === 0
+      ? dayName + ' (hoy)'
+      : dayName;
+  });
   
 export const dayToIndex = (day) => {
   const parsed = parseInt(day);
