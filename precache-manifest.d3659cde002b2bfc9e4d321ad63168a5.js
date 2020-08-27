dae50ccecf6cdf1c9b76dc3b729be10549b81fc9
agj <ale@agj.cl>
2020-08-27 19:20:36 -0400

    Updates

---
+++ precache-manifest.d3659cde002b2bfc9e4d321ad63168a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66ba65068924f76b8abdfa3ff6129e55",
+    "url": "/web-app-exercise/index.html"
+  },
+  {
+    "revision": "d4674229f91ace532c52",
+    "url": "/web-app-exercise/static/css/main.28b823ac.chunk.css"
+  },
+  {
+    "revision": "9628a457ca4a6523090e",
+    "url": "/web-app-exercise/static/js/2.5056fde0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/web-app-exercise/static/js/2.5056fde0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4674229f91ace532c52",
+    "url": "/web-app-exercise/static/js/main.6ce3aaba.chunk.js"
+  },
+  {
+    "revision": "402413971caf20868673",
+    "url": "/web-app-exercise/static/js/runtime-main.20be92e4.js"
+  }
+]);
